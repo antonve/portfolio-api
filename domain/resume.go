c478fc8f7d1de1dbeda8c8ae656f8926ff7f9367
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-02-20 13:39:00 +0900

    add resume domain

---
+++ domain/resume.go
@@ -0,0 +1,35 @@
+package domain
+
+import (
+	"github.com/srvc/fail"
+)
+
+type Resume struct {
+	Slug    string `json:"slug" db:"slug"`
+	Body    string `json:"body" db:"body"`
+	Enabled bool   `json:"enabled" db:"enabled"`
+}
+
+type ResumeLog struct {
+	ID        uint64 `json:"id" db:"id"`
+	Slug      string `json:"slug" db:"slug"`
+	IPAddress string `json:"ip_address" db:"ip_address"`
+	UserAgent string `json:"user_agent" db:"user_agent"`
+}
+
+func (r ResumeLog) Validate() (bool, error) {
+	if r.ID == 0 {
+		return false, fail.New("invalid `ID` supplied")
+	}
+	if len(r.Slug) == 0 {
+		return false, fail.New("no `Slug` supplied")
+	}
+	if len(r.IPAddress) == 0 {
+		return false, fail.New("invalid `IPAddress` supplied")
+	}
+	if len(r.UserAgent) == 0 {
+		return false, fail.New("invalid `UserAgent` supplied")
+	}
+
+	return true, nil
+}
