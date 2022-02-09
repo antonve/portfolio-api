75e84dbb2d60e5404bed6aadb6e58ba7b568c8a4
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-02-09 20:44:25 +0900

    fix migrations

--- migrations/202202092013_rename_resume_up.sql
+++ migrations/202202092013_rename_resume_up.sql
@@ -2,10 +2,9 @@ ALTER TABLE resume
 RENAME COLUMN enabled TO is_visible;
 
 CREATE TABLE resume_visits (
-  uuid uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   slug varchar(10) NOT NULL,
   created_at timestamp NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
   ip_address varchar(45) NOT NULL,
-  user_agent text NOT NULL,
-  PRIMARY KEY (id)
+  user_agent text NOT NULL
 );
